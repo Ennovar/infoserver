fb3021a06f5e0694d0777b02f75b325c34f937e5
Tanner Storment <tannerstorment@gmail.com>
2016-07-21 14:48:12 -0500

    Added different weights to roboto

--- links.php
+++ links.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" href="styling.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700' rel='stylesheet' type='text/css'>
 <script src="jquery-1.12.4.min.js"></script>
 <script src="msg.js"></script>
