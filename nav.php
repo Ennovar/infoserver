149463046b446b706a3257cce0243a1db9faffb8
Tanner Storment <tannerstorment@gmail.com>
2016-07-22 11:14:33 -0500

    Added navbar

---
+++ nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <button type="button" class="navbar-toggle collapsed" data-togle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="view-announcements.php">Announcements</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li><a href="view-announcements.php">View Announcements</a></li>
+            <li><a href="new-announcement.php">New Announcement</a></li>
+
+        </ul>
+    </div>
+</nav>
