c502beabf5052516551f85941499723cb9f81913
Tanner Storment <tannerstorment@gmail.com>
2016-07-27 13:51:51 -0500

    Sample config.php, the server still uses ./config.php though.

---
+++ config-sample.php
@@ -0,0 +1,12 @@
+<?php
+$mysql = [
+    "host" => "localhost",
+    "port" => 3306,
+    "db" => "db",
+    "user" => "user",
+    "password" => "password"
+];
+
+$images = [
+    "dir" => "/path/to/images/folder"
+];
